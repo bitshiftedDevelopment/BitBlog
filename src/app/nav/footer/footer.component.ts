24a80b579e601850fefb2ef673f62217eca5bb7d
Mick Evans <bitshifted.development@gmail.com>
2018-04-11 15:31:37 -0400

    Initial commit for footer component

---
+++ src/app/nav/footer/footer.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent implements OnInit {
+  estYear: string = 2018;
+  curYear: string;
+  @Input() author: string;
+  constructor() { }
+
+  ngOnInit() {
+    this.setCurYear();
+  }
+
+  private getCurrentYear() {
+    return (new Date()).getFullYear();
+  }
+  private setCurYear() {
+    this.curYear = this.getCurrentYear();
+  }
+
+}
