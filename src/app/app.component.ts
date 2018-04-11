fd144d4149079d23562effad86996b75dca009fd
Mick Evans <bitshifted.development@gmail.com>
2018-04-11 15:39:47 -0400

    Added author for injection into footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'My New Blog';
+  author = 'Your name here';
 }
