49d0d6624c78b185ea4f5e9294f146d29d51b6f2
Mick Evans <bitshifted.development@gmail.com>
2018-04-11 11:11:09 -0400

    Commented out auth service until implemented

--- src/app/pages/post/post.component.ts
+++ src/app/pages/post/post.component.ts
@@ -1,25 +1,26 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
-import { AuthService } from '@app/auth/auth.service';
-import { Post } from '../post';
+//import { AuthService } from '@app/auth/auth.service';
+import { Post } from './post';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 
 
 @Component({
-  selector: 'app-post',
+  selector: 'blog-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss']
 })
 export class PostComponent implements OnInit {
   @Input() post: Observable<Post>;// Used for injecting a post in the post-browser
-  constructor(private afs: AngularFirestore, public auth: AuthService, private route: ActivatedRoute, ) {
-
-
-    ngOnInit() {
-      this.route.params.subscribe(params => {
-        // Sets the current post. Cast the return to prevent typecasting errors
-        this.post = this.afs.doc('posts/' + params['id']).valueChanges() as Observable<Post>
-      });
-
-    }
+  constructor(
+    private afs: AngularFirestore,
+    //public auth: AuthService,
+    private route: ActivatedRoute, ) { }
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      // Sets the current post. Cast the return to prevent typecasting errors
+      this.post = this.afs.doc('posts/' + params['id']).valueChanges() as Observable<Post>
+    });
+  }
+}
