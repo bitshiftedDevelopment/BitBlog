bf8fb3b6f0a47f22fa3ffcd05a0e770e6aa77c9e
Mick Evans <bitshifted.development@gmail.com>
2018-04-11 11:08:15 -0400

    Added Material Design module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MaterialAppModule } from './ngmat/ngmat.module';
+
 import { AppRoutingModule } from './app-routing.module'; // Provides app routing
 import { AppComponent } from './app.component'; // Entry point into app
 import { PagesModule } from './pages/pages.module'; // Provides app structure
@@ -14,6 +16,7 @@ import { NavModule } from './nav/nav.module'; // Provides navigation components
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MaterialAppModule,
     PagesModule,
     NavModule
   ],
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MaterialAppModule } from '@app/ngmat/ngmat.module';
+
 import { HomeComponent } from './home/home.component'; // Landing page, gets replaced dynamically by RouterModule
 import { ListComponent } from './list/list.component'; // List of all posts that match a given search *NOT IMPLEMENTED YET*
 import { PostComponent } from './post/post.component'; // Single post layout component
@@ -8,7 +10,8 @@ import { PostComponent } from './post/post.component'; // Single post layout com
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialAppModule
   ],
   exports: [
     HomeComponent,
